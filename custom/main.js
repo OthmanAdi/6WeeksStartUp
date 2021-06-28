28faea6cf421ad11e7d843ce202dc60e3d13a261
Othman Adi <github_me@outlook.de>
2021-06-28 13:05:15 +0200

    MediaQuery and New Funktion OnCLick Week Section

--- custom/main.js
+++ custom/main.js
@@ -1,24 +1,32 @@
 var $cont = document.querySelector('.cont');
 var $elsArr = [].slice.call(document.querySelectorAll('.el'));
 var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
-var $closeBtnsDiv = document.getElementsByTagName('body')
+var $closeBtnsDiv = [].slice.call(document.querySelectorAll('.content-wrapper'));
 
-setTimeout(function() {
-  $cont.classList.remove('s--inactive');
+setTimeout(function () {
+    $cont.classList.remove('s--inactive');
 }, 200);
 
-$elsArr.forEach(function($el) {
-  $el.addEventListener('click', function() {
-    if (this.classList.contains('s--active')) return;
-    $cont.classList.add('s--el-active');
-    this.classList.add('s--active');
-  });
+$elsArr.forEach(function ($el) {
+    $el.addEventListener('click', function () {
+        if (this.classList.contains('s--active')) return;
+        $cont.classList.add('s--el-active');
+        this.classList.add('s--active');
+    });
 });
 
-$closeBtnsArr.forEach(function($btn) {
-  $btn.addEventListener('click', function(e) {
-    e.stopPropagation();
-    $cont.classList.remove('s--el-active');
-    document.querySelector('.el.s--active').classList.remove('s--active');
-  });
+$closeBtnsArr.forEach(function ($btn) {
+    $btn.addEventListener('mousedown', function (e) {
+        e.stopPropagation();
+        $cont.classList.remove('s--el-active');
+        document.querySelector('.el.s--active').classList.remove('s--active');
+    });
+});
+
+$closeBtnsDiv.forEach(function ($body) {
+    $body.addEventListener('mousedown', function (e) {
+        e.stopPropagation();
+        $cont.classList.remove('s--el-active');
+        document.querySelector('.el.s--active').classList.remove('s--active');
+    });
 });
